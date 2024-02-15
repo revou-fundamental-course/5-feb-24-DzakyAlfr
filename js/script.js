82d727e96b8b71c7e3bb6a3b42b8ae11601c2dbc
DzakyAlfr <dzakyalfitra@gmail.com>
2024-02-15 20:08:57 +0700

    update cara penyelesaian javascript

--- js/script.js
+++ js/script.js
@@ -6,43 +6,34 @@ var btnHasil = document.getElementById("hasilButton");
 var btnRumus = document.getElementById("konversi");
 var hasil = document.getElementById("textHasil")
 
+// Kalkulasi dari celcius ke fahrenheit
 celciusInput.addEventListener('input',function(){
     let celciusValue = (parseFloat(celciusInput.value) * 9/5)
     + 32;
-    //const celciusValue = celciusInput.value
     fahrenheitInput.value = parseFloat(celciusValue.toFixed(2));
     fahrenheitInput.innerText = celciusValue
 })
 
+// Kalkulasi dari fahrenheit ke celcius
 fahrenheitInput.addEventListener('input',function(){
     let fahrenheitValue = (parseFloat(fahrenheitInput.value)
     - 32)* 5/9;
-    //const celciusValue = celciusInput.value
     celciusInput.value = parseFloat(fahrenheitValue.toFixed(2));
     celciusInput.innerText = fahrenheitValue
 
 })
 
+// button untuk mereset semua perubahan
 btnHasil.addEventListener('click',function(){
     celciusInput.value = ""
     fahrenheitInput.value = ""
     hasil.textContent = ""
 })
 
+// button untuk menampilkan cara penyelesaian
 btnRumus.addEventListener('click',function(){
-    var hasilAkhir = ("(",celciusInput.value, "x 9/5 ) + 32 =", 
-    fahrenheitInput.value)
+    
+    var hasilAkhir = "(" + celciusInput.value + "°C x 9/5) + 32 = "
+    + fahrenheitInput.value + "°F";
     hasil.textContent = hasilAkhir;
 })
-
-// function celToFar(){
-//     let output = (parseFloat(celciusInput.value) * 9/5) + 32;
-//     fahrenheitInput.value = parseFloat(output.toFixed(2));
-//     console.log(output);
-// }
-
-// function farToCel(){
-//     let output = (parseFloat(fahrenheitInput.value) - 32) * 9/5;
-//     celciusInput.value = parseFloat(output.toFixed(2));
-//     console.log(output);
-// }
